26ada0c468f8a8f7bf8a05b927774117572e1ba6
혀느현스 <hyuns@hyuns.dev>
2024-02-26 05:28:54 +0900

    fix: fix type error (v1.0.13)

--- src/object/sectionPreview.object.ts
+++ src/object/sectionPreview.object.ts
@@ -1,27 +1,30 @@
 export class SectionPreviewObject {
+  id: number;
+  title: string;
+  bookId: number;
+  notionPageId: string;
+  parent?: SectionPreviewObject;
+  children?: SectionPreviewObject[];
+  order: number;
+  cachedAt: string;
+
+  constructor(data: {
     id: number;
     title: string;
     bookId: number;
+    notionPageId: string;
     parent?: SectionPreviewObject;
     children?: SectionPreviewObject[];
     order: number;
     cachedAt: string;
-
-    constructor(data: {
-        id: number;
-        title: string;
-        bookId: number;
-        parent?: SectionPreviewObject;
-        children?: SectionPreviewObject[];
-        order: number;
-        cachedAt: string;
-    }) {
-        this.id = data.id;
-        this.title = data.title;
-        this.bookId = data.bookId;
-        this.parent = data.parent;
-        this.children = data.children;
-        this.order = data.order;
-        this.cachedAt = data.cachedAt;
-    }
+  }) {
+    this.id = data.id;
+    this.title = data.title;
+    this.bookId = data.bookId;
+    this.notionPageId = data.notionPageId;
+    this.parent = data.parent;
+    this.children = data.children;
+    this.order = data.order;
+    this.cachedAt = data.cachedAt;
+  }
 }
