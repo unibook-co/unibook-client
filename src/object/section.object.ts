d80a63899ef68bde0e1cbbddfc6d544f74bca97b
혀느현스 <hyuns@hyuns.dev>
2024-02-25 16:41:36 +0900

    feat: SectionObject 수정 1.0.10

--- src/object/section.object.ts
+++ src/object/section.object.ts
@@ -1,33 +1,35 @@
 import { SectionPreviewObject } from "./sectionPreview.object";
 
 export class SectionObject {
+  id: number;
+  title: string;
+  bookId: number;
+  notionPageId: string;
+  recordMap?: any;
+  parent?: SectionPreviewObject;
+  children?: SectionPreviewObject[];
+  order: number;
+  cachedAt: string;
+
+  constructor(data: {
     id: number;
     title: string;
     bookId: number;
+    notionPageId: string;
     recordMap?: any;
     parent?: SectionPreviewObject;
     children?: SectionPreviewObject[];
     order: number;
     cachedAt: string;
-
-    constructor(data: {
-        id: number;
-        title: string;
-        bookId: number;
-        notionPageId: string;
-        recordMap?: any;
-        parent?: SectionPreviewObject;
-        children?: SectionPreviewObject[];
-        order: number;
-        cachedAt: string;
-    }) {
-        this.id = data.id;
-        this.title = data.title;
-        this.bookId = data.bookId;
-        this.recordMap = data.recordMap;
-        this.parent = data.parent;
-        this.children = data.children;
-        this.order = data.order;
-        this.cachedAt = data.cachedAt;
-    }
+  }) {
+    this.id = data.id;
+    this.title = data.title;
+    this.bookId = data.bookId;
+    this.notionPageId = data.notionPageId;
+    this.recordMap = data.recordMap;
+    this.parent = data.parent;
+    this.children = data.children;
+    this.order = data.order;
+    this.cachedAt = data.cachedAt;
+  }
 }
