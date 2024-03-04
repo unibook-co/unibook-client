833401ee4b8212b834dab33bf33a311373cd964f
혀느현스 <hyuns@hyuns.dev>
2024-03-04 16:49:34 +0900

    feat: AdminBookObject 업데이트

--- src/object/adminBook.object.ts
+++ src/object/adminBook.object.ts
@@ -1,4 +1,20 @@
 export class AdminBookObject {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  price: number;
+  coverImage: string;
+  notionDatabaseId: string;
+  status: "draft" | "pending" | "published" | "deleted";
+  authorUsers: {
+    id: number;
+    role: string;
+  }[];
+  createdAt: string;
+  updatedAt: string;
+
+  constructor(data: {
     id: number;
     title: string;
     author: string;
@@ -7,30 +23,23 @@ export class AdminBookObject {
     coverImage: string;
     notionDatabaseId: string;
     status: "draft" | "pending" | "published" | "deleted";
+    authorUsers: {
+      id: number;
+      role: string;
+    }[];
     createdAt: string;
     updatedAt: string;
-
-    constructor(data: {
-        id: number;
-        title: string;
-        author: string;
-        description: string;
-        price: number;
-        coverImage: string;
-        notionDatabaseId: string;
-        status: "draft" | "pending" | "published" | "deleted";
-        createdAt: string;
-        updatedAt: string;
-    }) {
-        this.id = data.id;
-        this.title = data.title;
-        this.author = data.author;
-        this.description = data.description;
-        this.price = data.price;
-        this.coverImage = data.coverImage;
-        this.notionDatabaseId = data.notionDatabaseId;
-        this.status = data.status;
-        this.createdAt = data.createdAt;
-        this.updatedAt = data.updatedAt;
-    }
+  }) {
+    this.id = data.id;
+    this.title = data.title;
+    this.author = data.author;
+    this.description = data.description;
+    this.price = data.price;
+    this.coverImage = data.coverImage;
+    this.notionDatabaseId = data.notionDatabaseId;
+    this.status = data.status;
+    this.authorUsers = data.authorUsers;
+    this.createdAt = data.createdAt;
+    this.updatedAt = data.updatedAt;
+  }
 }
