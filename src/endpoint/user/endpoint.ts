661bda3a8b82a7b74a370197f14bc3c069649228
혀느현스 <hyuns@hyuns.dev>
2024-03-07 21:08:21 +0900

    feat: IssueToken, CreateUser 추가 1.0.25

--- src/endpoint/user/endpoint.ts
+++ src/endpoint/user/endpoint.ts
@@ -6,18 +6,18 @@ import { UserObject } from "../../object/user.object";
  * 유저를 조회합니다.
  */
 export const ListUser: Endpoint<ListUserReq, ListUserRes> = {
-    method: "GET",
-    path: (e) => `/users`,
-    queryParams: ["page", "take"],
+  method: "GET",
+  path: (e) => `/users`,
+  queryParams: ["page", "take"],
 };
 export type ListUserReqQuery = {
-    page?: number;
-    take?: number;
+  page?: number;
+  take?: number;
 };
-export type ListUserReq = ListUserReqQuery
+export type ListUserReq = ListUserReqQuery;
 export type ListUserRes = {
-    users: UserObject[];
-    total: number;
+  users: UserObject[];
+  total: number;
 };
 
 /**
@@ -25,16 +25,34 @@ export type ListUserRes = {
  * 유저를 조회합니다.
  */
 export const GetUser: Endpoint<GetUserReq, GetUserRes> = {
-    method: "GET",
-    path: (e) => `/users/${e.userId}`,
-    pathParams: ["userId"],
+  method: "GET",
+  path: (e) => `/users/${e.userId}`,
+  pathParams: ["userId"],
 };
 export type GetUserReqPath = {
-    userId: number | string;
+  userId: number | string;
 };
 export type GetUserReq = GetUserReqPath;
 export type GetUserRes = {
-    user: UserObject;
+  user: UserObject;
+};
+
+/**
+ * POST /users
+ * 유저를 생성합니다.
+ */
+export const CreateUser: Endpoint<CreateUserReq, CreateUserRes> = {
+  method: "POST",
+  path: (e) => `/users`,
+  bodyParams: ["name", "email", "profileImage"],
+};
+export type CreateUserReq = {
+  name: string;
+  email: string;
+  profileImage?: string;
+};
+export type CreateUserRes = {
+  user: UserObject;
 };
 
 /**
@@ -42,16 +60,16 @@ export type GetUserRes = {
  * 유저를 검색합니다.
  */
 export const SearchUser: Endpoint<SearchUserReq, SearchUserRes> = {
-    method: "GET",
-    path: (e) => `/users/search`,
-    queryParams: ["name", "email"],
+  method: "GET",
+  path: (e) => `/users/search`,
+  queryParams: ["name", "email"],
 };
 export type SearchUserReq = {
-    name?: string;
-    email?: string;
+  name?: string;
+  email?: string;
 };
 export type SearchUserRes = {
-    users: UserObject[];
+  users: UserObject[];
 };
 
 /**
@@ -59,20 +77,20 @@ export type SearchUserRes = {
  * 유저를 수정합니다.
  */
 export const UpdateUser: Endpoint<UpdateUserReq, UpdateUserRes> = {
-    method: "PATCH",
-    path: (e) => `/users/${e.userId}`,
-    pathParams: ["userId"],
-    bodyParams: ["name", "profileImage"],
+  method: "PATCH",
+  path: (e) => `/users/${e.userId}`,
+  pathParams: ["userId"],
+  bodyParams: ["name", "profileImage"],
 };
 export type UpdateUserReqPath = {
-    userId: number | string;
+  userId: number | string;
 };
 export type UpdateUserReq = UpdateUserReqPath & {
-    name?: string;
-    profileImage?: string;
+  name?: string;
+  profileImage?: string;
 };
 export type UpdateUserRes = {
-    user: UserObject;
+  user: UserObject;
 };
 
 /**
@@ -80,25 +98,25 @@ export type UpdateUserRes = {
  * 유저 프로필 이미지를 수정합니다.
  */
 export const UpdateUserProfileImage: Endpoint<
-    UpdateUserProfileImageReq,
-    UpdateUserProfileImageRes
-    > = {
-    method: "PATCH",
-    path: (e) => `/users/${e.userId}/profile-image`,
-    pathParams: ["userId"],
-    bodyParams: ["file"],
-    headers: {
-        "Content-Type": "multipart/form-data",
-    }
+  UpdateUserProfileImageReq,
+  UpdateUserProfileImageRes
+> = {
+  method: "PATCH",
+  path: (e) => `/users/${e.userId}/profile-image`,
+  pathParams: ["userId"],
+  bodyParams: ["file"],
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
 };
 export type UpdateUserProfileImageReqPath = {
-    userId: number | string;
+  userId: number | string;
 };
 export type UpdateUserProfileImageReq = UpdateUserProfileImageReqPath & {
-    file: any;
+  file: any;
 };
 export type UpdateUserProfileImageRes = {
-    user: UserObject;
+  user: UserObject;
 };
 
 /**
@@ -106,12 +124,12 @@ export type UpdateUserProfileImageRes = {
  * 유저를 삭제합니다.
  */
 export const DeleteUser: Endpoint<DeleteUserReq, DeleteUserRes> = {
-    method: "DELETE",
-    path: (e) => `/users/${e.userId}`,
-    pathParams: ["userId"],
+  method: "DELETE",
+  path: (e) => `/users/${e.userId}`,
+  pathParams: ["userId"],
 };
 export type DeleteUserReqPath = {
-    userId: number | string;
+  userId: number | string;
 };
 export type DeleteUserReq = DeleteUserReqPath;
 export type DeleteUserRes = {};
