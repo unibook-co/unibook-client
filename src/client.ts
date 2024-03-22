d759def8b9228fa0f0cba7b147527d8d0a2ba016
혀느현스 <hyuns@hyuns.dev>
2024-03-22 15:21:41 +0900

    feat: 주석 추가

--- src/client.ts
+++ src/client.ts
@@ -69,12 +69,18 @@ import {
 } from "./endpoint";
 
 export class UniBookClient extends EndpointClient {
+  /**
+   * Notion Page
+   */
   readonly LoadNotionPage = this.endpointBuilder(LoadNotionPage);
   readonly ReloadNotionPage = this.endpointBuilder(ReloadNotionPage);
   readonly DeleteNotionPage = this.endpointBuilder(DeleteNotionPage);
   readonly ListNotionPage = this.endpointBuilder(ListNotionPage);
   readonly GetNotionPage = this.endpointBuilder(GetNotionPage);
 
+  /**
+   * User
+   */
   readonly ListUser = this.endpointBuilder(ListUser);
   readonly GetUser = this.endpointBuilder(GetUser);
   readonly CreateUser = this.endpointBuilder(CreateUser);
@@ -85,9 +91,11 @@ export class UniBookClient extends EndpointClient {
   );
   readonly DeleteUser = this.endpointBuilder(DeleteUser);
 
+  // User Setting
   readonly GetUserSetting = this.endpointBuilder(GetUserSetting);
   readonly UpdateUserSetting = this.endpointBuilder(UpdateUserSetting);
 
+  // User Device
   readonly ListUserDevice = this.endpointBuilder(ListUserDevice);
   readonly DeleteUserDevice = this.endpointBuilder(DeleteUserDevice);
 
@@ -96,10 +104,12 @@ export class UniBookClient extends EndpointClient {
     DeleteUserNotification
   );
 
+  // User Notification
   readonly RegisterUserNotificationDevice = this.endpointBuilder(
     RegisterUserNotificationDevice
   );
 
+  // User Order
   readonly CreateUserOrder = this.endpointBuilder(CreateUserOrder);
   readonly ApproveUserOrder = this.endpointBuilder(ApproveUserOrder);
   readonly CancelUserOrder = this.endpointBuilder(CancelUserOrder);
@@ -108,12 +118,19 @@ export class UniBookClient extends EndpointClient {
     CreateAndApproveCashUserOrder
   );
 
+  // User Book
   readonly ListUserBook = this.endpointBuilder(ListUserBook);
 
+  /**
+   * Auth
+   */
   readonly AuthOAuthGoogle = this.endpointBuilder(AuthOAuthGoogle);
 
   readonly IssueToken = this.endpointBuilder(IssueToken);
 
+  /**
+   * Book
+   */
   readonly ListBook = this.endpointBuilder(ListBook);
   readonly ListAdminBook = this.endpointBuilder(ListAdminBook);
   readonly GetBook = this.endpointBuilder(GetBook);
@@ -123,10 +140,14 @@ export class UniBookClient extends EndpointClient {
   readonly UpdateBook = this.endpointBuilder(UpdateBook);
   readonly DeleteBook = this.endpointBuilder(DeleteBook);
 
+  // Book Author
   readonly AddBookAuthor = this.endpointBuilder(AddBookAuthor);
   readonly UpdateBookAuthor = this.endpointBuilder(UpdateBookAuthor);
   readonly DeleteBookAuthor = this.endpointBuilder(DeleteBookAuthor);
 
+  /**
+   * Section
+   */
   readonly ListSection = this.endpointBuilder(ListSection);
   readonly GetSection = this.endpointBuilder(GetSection);
   readonly GetIntroductionSection = this.endpointBuilder(
@@ -137,13 +158,16 @@ export class UniBookClient extends EndpointClient {
   readonly UpdateSection = this.endpointBuilder(UpdateSection);
   readonly DeleteSection = this.endpointBuilder(DeleteSection);
 
+  // Section Export
   readonly ExportSectionPDF = this.endpointBuilder(ExportSectionPDF);
 
+  // Section Block Comment
   readonly ListBlockComment = this.endpointBuilder(ListBlockComment);
   readonly CreateBlockComment = this.endpointBuilder(CreateBlockComment);
   readonly EditBlockComment = this.endpointBuilder(EditBlockComment);
   readonly DeleteBlockComment = this.endpointBuilder(DeleteBlockComment);
 
+  // Section Block Comment Reaction
   readonly ListBlockCommentReaction = this.endpointBuilder(
     ListBlockCommentReaction
   );
@@ -154,6 +178,7 @@ export class UniBookClient extends EndpointClient {
     DeleteBlockCommentReaction
   );
 
+  // Section Block Comment Notification
   readonly GetIsBlockCommentNotificationOn = this.endpointBuilder(
     GetIsBlockCommentNotificationOn
   );
@@ -164,17 +189,25 @@ export class UniBookClient extends EndpointClient {
     SetBlockCommentNotificationOff
   );
 
+  // Section Block Memo
   readonly ListBlockMemo = this.endpointBuilder(ListBlockMemo);
   readonly CreateBlockMemo = this.endpointBuilder(CreateBlockMemo);
   readonly UpdateBlockMemo = this.endpointBuilder(UpdateBlockMemo);
   readonly DeleteBlockMemo = this.endpointBuilder(DeleteBlockMemo);
 
+  // Section Bookmark
   readonly ListBookmark = this.endpointBuilder(ListBookmark);
   readonly CreateBookmark = this.endpointBuilder(CreateBookmark);
   readonly DeleteBookmark = this.endpointBuilder(DeleteBookmark);
 
+  /**
+   * Order
+   */
   readonly GetOrder = this.endpointBuilder(GetOrder);
   readonly ListOrder = this.endpointBuilder(ListOrder);
 
+  /**
+   * File
+   */
   readonly UploadFile = this.endpointBuilder(UploadFile);
 }
